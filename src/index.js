4c5090ea5a05953cd6dd942422cb87a95723f697
MarynaPrusenka <upgrade-course@mail.ru>
2023-01-22 19:46:20 +0300

    feat: update a task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
-}
+    let n = 261;
+    // Function to reverse digits of num
+    function reverse (n) {
+        let rev_n= 0;
+        while(n > 0)
+        {
+            rev_n = rev_n * 10 + n % 10;
+            n = Math.floor(n / 10);
+        }
+        return rev_n;
+    }
